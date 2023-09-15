a28b0f79e10d2815f9b2cb4003c23f090b7b40b4
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2023-09-15 16:17:52 +0900

    Feat: 레이아웃, 네비게이션

---
+++ components/base/Layout.jsx
@@ -0,0 +1,31 @@
+import { Main } from 'next/document';
+import React from 'react';
+import styled from 'styled-components';
+import Navigation from './Navigation';
+
+const Layout = ({ children }) => {
+  return (
+    <Container>
+      <main>{children}</main>
+      <Navigation />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  background-color: ${({ theme }) => theme.colors.backgroundColor};
+  width: 576px;
+  min-height: 100vh;
+  margin: 0 auto;
+  main {
+    padding: 16px 40px 0;
+    @media ${({ theme }) => theme.devices.mobile} {
+      padding: 16px 20px 0;
+    }
+  }
+  @media ${({ theme }) => theme.devices.mobile} {
+    width: 100%;
+  }
+`;
+
+export default Layout;
