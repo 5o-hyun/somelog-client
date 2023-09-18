4afa0908c8b78432aaf7d4fa654ac8ccbb3dadba
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2023-09-18 17:11:39 +0900

    Feature: 일정 조회

--- components/schedule/Calender.jsx
+++ components/schedule/Calender.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 import { Calendar } from 'antd';
 
-const Calender = ({ onPanelChange, handleDateSelect }) => {
+const Calender = ({ onPanelChange, handleDateSelect, cellRender }) => {
   return (
     <>
       <Container>
-        <Calendar onPanelChange={onPanelChange} onSelect={handleDateSelect} />
+        <Calendar
+          onPanelChange={onPanelChange}
+          onSelect={handleDateSelect}
+          cellRender={cellRender}
+        />
       </Container>
     </>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,22 @@
 import { GlobalStyles } from '@styles/GlobalStyles';
 import theme from '@styles/theme';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import { ThemeProvider } from 'styled-components';
 
+import axios from 'axios';
+axios.defaults.withCredentials = true;
+
 function MyApp({ Component, pageProps }) {
+  const queryClient = new QueryClient();
+
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
