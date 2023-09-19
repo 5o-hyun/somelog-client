0a7174e4d9fe6a7ac42ca6ce07f38f16b3852373
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2023-09-19 19:58:44 +0900

    Design: 캘린더 카테고리

--- components/schedule/Category.jsx
+++ components/schedule/Category.jsx
@@ -1,24 +1,47 @@
-import React, { useState } from 'react';
-import { Button, Drawer, Radio, Space } from 'antd';
+import React from 'react';
+import { Badge, Drawer, Space } from 'antd';
 import styled from 'styled-components';
+
+const colors = [
+  'pink',
+  'red',
+  'yellow',
+  'orange',
+  'cyan',
+  'green',
+  'blue',
+  'purple',
+  'geekblue',
+  'magenta',
+  'volcano',
+  'gold',
+  'lime',
+];
 
 const Category = ({ onToggleCategory }) => {
   return (
     <StyledDrawer
-      title="Basic Drawer"
+      title="카테고리 선택"
       placement="bottom"
       closable={false}
       onClose={onToggleCategory}
       open
     >
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
+      <Space direction="vertical">
+        {colors.map((color) => (
+          <Badge key={color} color={color} text={color} />
+        ))}
+      </Space>
     </StyledDrawer>
   );
 };
 const StyledDrawer = styled(Drawer)`
-  position: absolute;
+  width: 576px !important;
+  margin: 0 auto;
+  border-radius: 8px 8px 0 0;
+  @media ${({ theme }) => theme.devices.mobile} {
+    width: 100% !important;
+  }
 `;
 
 export default Category;
