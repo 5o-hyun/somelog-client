28c9a40061b182d058f09694b4721e323d6c2077
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2023-10-31 11:54:34 +0900

    Remove: .

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-import Layout from '@components/base/Layout';
-
-export default function Home() {
-  return <Layout>home</Layout>;
-}
--- styles/antdTheme.js
+++
@@ -1,6 +0,0 @@
-export const antdTheme = {
-  token: {
-    colorPrimary: '#ffc8c8',
-    fontFamily: 'ACCchildrenheartOTF-Regular',
-  },
-};
