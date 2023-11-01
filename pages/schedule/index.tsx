4bf1ab056d1cc3705b92e3289efc9c55d92b8e41
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2023-11-01 13:49:00 +0900

    Feat: 페이지 구조설정

---
+++ pages/schedule/index.tsx
@@ -0,0 +1,8 @@
+import ScheduleContainer from '@container/schedule/ScheduleContainer';
+import React from 'react';
+
+const index = () => {
+  return <ScheduleContainer />;
+};
+
+export default index;
