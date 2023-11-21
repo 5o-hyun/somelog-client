1f186a96c248f8f9e52c2154ad5407800171cfdb
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2023-11-21 19:26:51 +0900

    Test: socket

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
+import socket from '@lib/socket';
+
 import Layout from '@components/base/Layout';
 
 import { GlobalStyles } from '@styles/GlobalStyles';
 import { antdTheme } from '@styles/antdTheme';
 import theme from '@styles/theme';
 
-import socket from './server';
 import { ConfigProvider } from 'antd';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
@@ -12,6 +13,8 @@ import { ThemeProvider } from 'styled-components';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
+
+  socket.on('connect', () => console.log('connect'));
 
   return (
     <QueryClientProvider client={queryClient}>
