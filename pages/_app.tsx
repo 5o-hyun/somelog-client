b1974174c228ad4c0df73ed1f7918d92df72d1a2
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2023-11-22 16:33:31 +0900

    Feat: 로그인

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,13 +8,31 @@ import theme from '@styles/theme';
 
 import { ConfigProvider } from 'antd';
 import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from 'styled-components';
+
+socket.on('connect', () => console.log('connect'));
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
 
-  socket.on('connect', () => console.log('connect'));
+  const [user, setUser] = useState(null);
+  const askUserName = () => {
+    const userName = prompt('당신의 이름을 입력하세요');
+
+    // .emit(대화의제목,보낼내용,콜백함수(앞에처리가잘되었으면 response받는다))
+    socket.emit('login', userName, (res: any) => {
+      console.log('res', res);
+      if (res?.ok) {
+        setUser(res.data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    askUserName();
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
