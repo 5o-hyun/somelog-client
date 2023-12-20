903d6a33b4cd5c49605d236d062ced4bcd7f835a
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2023-12-20 15:53:22 +0900

    Chore: zustand devtools

--- stores/auth.ts
+++ stores/auth.ts
@@ -2,6 +2,7 @@
 import { User } from '@typess/user';
 
 import { create } from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
 
 interface AuthState {
   user: User | null;
@@ -9,10 +10,21 @@ interface AuthState {
   logoutUser: () => void;
 }
 
-const useAuthStore = create<AuthState>((set) => ({
-  user: null,
-  loginUser: (user) => set({ user }),
-  logoutUser: () => set({ user: null }),
-}));
+// const useAuthStore = create<AuthState>((set) => ({
+//   user: null,
+//   loginUser: (user) => set({ user }),
+//   logoutUser: () => set({ user: null }),
+// }));
+
+const useAuthStore = create<AuthState>()(
+  devtools(
+    (set) => ({
+      user: null,
+      loginUser: (user) => set({ user }),
+      logoutUser: () => set({ user: null }),
+    }),
+    { name: 'authStore' },
+  ),
+);
 
 export default useAuthStore;
