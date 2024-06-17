d31baa57a6261d7f308c5e398ccdd2f23f8d635c
5Kcoms <94782559+5o-hyun@users.noreply.github.com>
2024-06-17 18:49:36 +0900

    Chore: 채팅 임시처리

--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -5,11 +5,7 @@ import Layout from '@components/base/Layout';
 import React from 'react';
 
 const index = () => {
-  return (
-    <Layout>
-      <ChatContainer />
-    </Layout>
-  );
+  return <Layout>{/* <ChatContainer /> */}로그인 오류로인한 임시처리</Layout>;
 };
 
 export default index;
