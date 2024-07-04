b1beb8a28e812b1c6cbfe9093149259544eea67f
5Kcoms <94782559+5o-hyun@users.noreply.github.com>
2024-07-04 20:22:49 +0900

    Feat: 기념일 해제

---
+++ components/home/celebration/Celebration.tsx
@@ -0,0 +1,95 @@
+import { Schedules } from '@typess/schedule';
+
+import { DeleteOutlined } from '@ant-design/icons';
+
+import { Empty, Popconfirm } from 'antd';
+import dayjs from 'dayjs';
+import React from 'react';
+import styled from 'styled-components';
+
+interface CelebrationProps {
+  lists: Schedules;
+  onDelete: (id: number) => void;
+}
+
+const Celebration: React.FC<CelebrationProps> = ({ lists, onDelete }) => {
+  const onConfirmDelete = (id: number) => {
+    onDelete(id);
+  };
+  return (
+    <Container>
+      {lists ? (
+        lists.map((list) => (
+          <div key={list.id} className="wrapper">
+            <div className="info">
+              <img
+                src={`/images/home/celebration/${list.sticker}.png`}
+                alt="스티커"
+                className="celebrationSticker"
+              />
+              <p className="celebrationTitle">{list.title}</p>
+              <p className="celebrationDate">
+                {dayjs(list.startDate).format('YYYY.MM.DD')}
+              </p>
+            </div>
+            <Popconfirm
+              title="기념일을 해제하시겠습니까?"
+              description="기념일을 해제해도 일정은 삭제되지않습니다"
+              onConfirm={() => onConfirmDelete(list.id)}
+              okText="해제"
+              cancelText="취소"
+            >
+              <div className="deleteBtn">
+                <DeleteOutlined />
+              </div>
+            </Popconfirm>
+          </div>
+        ))
+      ) : (
+        <Empty description="데이터가 없습니다. 새로 등록해보세요!" />
+      )}
+    </Container>
+  );
+};
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  .wrapper {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    border: 1px solid #ddd;
+    padding: 4px;
+    border-radius: 12px;
+    .info {
+      flex: 1;
+      display: flex;
+      align-items: center;
+      gap: 12px;
+      padding: 4px 8px;
+      cursor: pointer;
+      .celebrationSticker {
+        width: 30px;
+      }
+      .celebrationTitle {
+        color: ${({ theme }) => theme.colors.TextColor};
+        font-size: 18px;
+      }
+      .celebrationDate {
+        color: ${({ theme }) => theme.colors.gray[500]};
+      }
+    }
+    .deleteBtn {
+      border: 1px solid ${({ theme }) => theme.colors.gray[200]};
+      padding: 8px;
+      border-radius: 12px;
+      cursor: pointer;
+      &:hover {
+        background-color: #ddd;
+      }
+    }
+  }
+`;
+
+export default Celebration;
--- container/home/CelebrationContainer.tsx
+++ container/home/CelebrationContainer.tsx
@@ -1,13 +1,45 @@
+import { deleteCelebration, getCelebrationList } from '@lib/api/schedule';
+
 import Title from '@components/base/Title';
+import Celebration from '@components/home/celebration/Celebration';
 import CelebrationToolbar from '@components/home/celebration/CelebrationToolbar';
 
+import useAuthStore from '@/stores/auth';
+
+import { message } from 'antd';
 import React from 'react';
+import { useMutation, useQuery } from 'react-query';
 
 const CelebrationContainer = () => {
+  const { user } = useAuthStore();
+  const { data: celebrations, refetch: refetchCelebrations } = useQuery(
+    ['celebrations', user?.id],
+    () => getCelebrationList(user?.id as number),
+    { enabled: !!user },
+  );
+  console.log(celebrations);
+
+  const deleteCelebrationMutation = useMutation(deleteCelebration, {
+    onSuccess: () => {
+      message.success('기념일을 해제했습니다.');
+      refetchCelebrations();
+    },
+    onError: (err: any) => {
+      err.response.data
+        ? message.error(err.response.data)
+        : message.error('기념일을 해제할수없습니다.');
+    },
+  });
+
+  const onDelete = (id: number) => {
+    deleteCelebrationMutation.mutate(id);
+  };
+
   return (
     <>
       <Title name="기념일관리" />
       <CelebrationToolbar />
+      <Celebration lists={celebrations} onDelete={onDelete} />
     </>
   );
 };
